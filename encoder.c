6718dbabe582256e73acdb932cd2a7074f6e9104
Hamza <karlchurchill@gmail.com>
2018-08-04 19:19:37 +0100

    Some cleaning up of names.

--- encoder.c
+++ encoder.c
@@ -13,48 +13,48 @@
  * 14-bit value is found and that is then used to work out the ideal Paula channel volume for this frame and what to
  * multiply each sample by before converting to 8-bit.
  */
-size_t encode_frame(const int16* input, int8* output, size_t framelen) {
-  const int16* source   = input;
-  size_t       num      = framelen;
-  int16        max14bit = 0;
+size_t encode_frame(const int16* input, int8* output, size_t frame_len) {
+  const int16* source    = input;
+  size_t       count     = frame_len;
+  int16        max_14bit = 0;
 
   /* Identify the loudest 14-bit sample value */
-  while (num--) {
-    int16 sample14bit = (*source++) >> 2;
-    int16 abs14bit    = sample14bit < 0        ? -sample14bit : sample14bit;
-    max14bit          = abs14bit    > max14bit ?  abs14bit    : max14bit;
+  while (count--) {
+    int16 cur_14bit = (*source++) >> 2;
+    int16 abs_14bit = cur_14bit < 0         ? -cur_14bit : cur_14bit;
+    max_14bit       = abs_14bit > max_14bit ?  abs_14bit : max_14bit;
   }
 
   /* If we comnputed a non-zero maximum 14-bit sample, work out the best AM volume to use and convert the frame */
-  if (max14bit) {
-    float32 ideal_factor = 8192.0 / (float32)max14bit;
+  if (max_14bit) {
+    float32 ideal_factor = 8192.0 / (float32)max_14bit;
     float32 best_factor  = 0;
-    int     index        = 0;
-    while (ideal_factor < amp_factors[index]) {
-      index++;
+    int     amp_index    = 0;
+    while (ideal_factor < amp_factors[amp_index]) {
+      amp_index++;
     }
 
-    best_factor = amp_factors[index];
-    int max8bit = (int)(max14bit * best_factor) >> 6;
+    best_factor = amp_factors[amp_index];
+    int max_8bit = (int)(max_14bit * best_factor) >> 6;
 
     printf(
       "\tMax 14-bit:%4d (ideal %0.6f) AUDxVOL:%d (scale: %0.6f), 8-bit:%d, replay: %d\n",
-      (int)max14bit,
+      (int)max_14bit,
       ideal_factor,
-      index + 1,
+      amp_index + 1,
       best_factor,
-      max8bit,
-      (int)((max8bit << 6) / best_factor)
+      max_8bit,
+      (int)((max_8bit << 6) / best_factor)
     );
 
     source    = input;
-    num       = framelen;
+    count     = frame_len;
 
     /* Write the AM volume value. This is in the range 1-64 for Paula */
-    *output++ = index + 1;
+    *output++ = amp_index + 1;
 
     /* Compute and write the maximised 8-bit samples */
-    while (num--) {
+    while (count--) {
       /* Calculate the next AM maximised 8-bit sample value */
       int val = (int)((float32)(*source++ >> 2) * best_factor) >> 6;
 
@@ -62,7 +62,7 @@ size_t encode_frame(const int16* input, int8* output, size_t framelen) {
       *output++ = val < -127 ? -127 : val > 127 ? 127 : val;
     }
 
-    return framelen + 1;
+    return frame_len + 1;
   } else {
     /* If we got a zero maximum 14-bit sample, record a single zero byte for the whole silent frame */
     puts("\tEncoded silent frame.");
